bfc918bcf702ab062e27f96768863c6a1b6bb0f0
taskinoz <tristan@taskinoz.com>
2018-12-10 23:56:23 +1100

    Added example login

---
+++ twitch-login.example.js
@@ -0,0 +1,5 @@
+module.exports = {
+  username:     'username', // Your Twitch username
+  oauth:     'oauth', // Your OAUTH - https://twitchapps.com/tmi/
+  channels: 'channels' // Your Twitch channel
+};
