98124eca146c0f3cf78688c481701e21b40adf44
taskinoz <tristan@taskinoz.com>
2018-12-12 00:28:18 +1100

    Added new commands

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const TwitchBot = require('twitch-bot'); //https://github.com/kritzware/twitch-bot
 const Login = require('./twitch-login.js');
+const ks = require('node-key-sender'); //https://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html
+
 
 //Login Info
 const Bot = new TwitchBot({
@@ -11,8 +13,21 @@ const Bot = new TwitchBot({
 const Commands = {
   lowGravity: ['sv_gravity 300','Low Gravity'],
   regularGravity: ['sv_gravity 750','Regular Gravity'],
-  highGravity: ['sv_gravity 900','High Gravity']
+  highGravity: ['sv_gravity 900','High Gravity'],
+  inverted: ['m_invert_pitch 1','Inverted'],
+  fov: ['cl_fovScale 0.5', 'Change FOV'],
+  difficultyEasy: ['sp_difficulty 0', 'Easy Difficulty'],
+  difficultyRegular: ['sp_difficulty 1', 'Regular Difficulty'],
+  difficultyHard: ['sp_difficulty 2', 'Hard Difficulty'],
+  difficultyMaster: ['sp_difficulty 3', 'Master Difficulty']
 }
+
+// FOV SCALE
+// 1.55 - 110
+// 1.42003 - 100
+// 1.28442 - 90
+// 1.13185 - 80
+// 1 - 70
 
 function generateCommands() {
   Bot.say("!1 - "+Commands.lowGravity[1]);
@@ -29,12 +44,15 @@ Bot.on('join', () => {
       switch (chatter.message) {
         case '!1':
           Bot.say("You have chosen 1");
+          ks.sendKey('equals');
           break;
         case '!2':
           Bot.say("You have chosen 2");
+          ks.sendKey('space');
           break;
         case '!3':
           Bot.say("You have chosen 3");
+          ks.sendKey('q');
           break;
         default:
           Bot.say("Sorry that isn't a command");
