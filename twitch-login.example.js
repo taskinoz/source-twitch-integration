4c9a0c1b1d15512562d8a192e4c32d51b8e8e2a4
taskinoz <tristan@taskinoz.com>
2018-12-19 01:04:24 +1100

    Converted module export to json

--- twitch-login.example.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  username:     'username', // Your Twitch username
-  oauth:     'oauth', // Your OAUTH - https://twitchapps.com/tmi/
-  channels: 'channels' // Your Twitch channel
-};
